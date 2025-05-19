302f07cc40d16d3ecdb3de79b98c3e4ea2649e28
Vu Nguyen Khoi <vunguyenkhoi47@gmail.com>
2025-05-19 17:55:04 +0700

    test jenkins

--- src/main/java/com/example/social_media/SocialMediaApplication.java
+++ src/main/java/com/example/social_media/SocialMediaApplication.java
@@ -14,6 +14,6 @@ public class SocialMediaApplication {
 	}
 	@GetMapping("/")
 	public String home() {
-		return "Welcome to My social media web";
+		return "Welcome to My social media web hehehehe";
 	}
 }
