b19bc9f82a390c3e9479cf182bad97de0eb86d6e
Vu Nguyen Khoi <vunguyenkhoi47@gmail.com>
2025-05-22 13:36:25 +0700

    test ci cd

--- src/main/java/com/example/social_media/SocialMediaApplication.java
+++ src/main/java/com/example/social_media/SocialMediaApplication.java
@@ -14,6 +14,6 @@ public class SocialMediaApplication {
 	}
 	@GetMapping("/")
 	public String home() {
-		return "Welcome to My social media web hehehehe";
+		return "Welcome to My social media web ai lam ma xin vcl";
 	}
 }
