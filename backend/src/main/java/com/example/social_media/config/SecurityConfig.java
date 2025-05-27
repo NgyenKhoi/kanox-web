f258e3b1becaca0a5f202948b84ad90193cc4fbb
Vu Nguyen Khoi <vunguyenkhoi47@gmail.com>
2025-05-28 01:02:28 +0700

    fix url from backend

--- backend/src/main/java/com/example/social_media/config/SecurityConfig.java
+++ backend/src/main/java/com/example/social_media/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://kanox-web.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
