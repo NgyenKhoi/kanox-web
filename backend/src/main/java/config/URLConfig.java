1e476b9a303168cc6c3841af10989e93299dc5bd
huyhoang <huyshadown937@gmail.com>
2025-05-30 16:17:34 +0700

    update URLConfig for page

--- backend/src/main/java/config/URLConfig.java
+++ backend/src/main/java/config/URLConfig.java
@@ -1,1 +1,10 @@
- 
+package config;
+
+public class URLConfig {
+    public static final String PAGE_BASE = "/api/pages";
+    public static final String PAGE_GET_ALL = PAGE_BASE;
+    public static final String PAGE_GET_BY_ID = PAGE_BASE + "/{id}";
+    public static final String PAGE_CREATE = PAGE_BASE;
+    public static final String PAGE_UPDATE = PAGE_BASE + "/{id}";
+    public static final String PAGE_DELETE = PAGE_BASE + "/{id}";
+} 
