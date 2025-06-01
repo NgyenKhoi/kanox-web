4744e90e7e2d11152b2bbf26bb3509c78fec39fc
Vu Nguyen Khoi <vunguyenkhoi47@gmail.com>
2025-06-02 02:05:55 +0700

    add google react json

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="233866118973-t26ue94egg2v1reebqpe684kglf0bjej.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
