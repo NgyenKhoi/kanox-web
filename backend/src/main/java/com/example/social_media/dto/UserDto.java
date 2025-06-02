1d283d5c721f16647bea8d66bac039dd5168165b
Vu Nguyen Khoi <vunguyenkhoi47@gmail.com>
2025-06-02 20:13:29 +0700

    add default constructor

--- backend/src/main/java/com/example/social_media/dto/UserDto.java
+++ backend/src/main/java/com/example/social_media/dto/UserDto.java
@@ -7,9 +7,18 @@ public class UserDto {
     private String displayName;
     private String email;
 
+    public UserDto() {}
+
     public UserDto(User user) {
         this.username = user.getUsername();
         this.displayName = user.getDisplayName();
         this.email = user.getEmail();
     }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getDisplayName() { return displayName; }
+    public void setDisplayName(String displayName) { this.displayName = displayName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
