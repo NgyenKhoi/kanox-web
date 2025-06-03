9997c3c9dd5a47de848e065a6a023033396a9967
Vu Nguyen Khoi <vunguyenkhoi47@gmail.com>
2025-06-04 03:07:33 +0700

    add nullable for phonenumber

--- backend/DATABASE.sql
+++ backend/DATABASE.sql
@@ -21,7 +21,7 @@
         id INT PRIMARY KEY IDENTITY(1, 1),
         email NVARCHAR(50) NOT NULL UNIQUE,
         username VARCHAR(30) NOT NULL UNIQUE,
-        phone_number VARCHAR(12) UNIQUE,
+        phone_number VARCHAR(12) UNIQUE null,
         password VARCHAR(255) NOT NULL,
         persistent_cookie VARCHAR(255),
         google_id VARCHAR(255),
