78449e110393b6276c019c1b77d10abb51e8096e
Vu Nguyen Khoi <vunguyenkhoi47@gmail.com>
2025-06-12 15:12:06 +0700

    fix

--- backend/src/main/java/com/example/social_media/dto/search/SearchResponseDto.java
+++ backend/src/main/java/com/example/social_media/dto/search/SearchResponseDto.java
@@ -3,12 +3,11 @@ package com.example.social_media.dto.search;
 import com.example.social_media.dto.user.GroupDto;
 import com.example.social_media.dto.user.PageDto;
 import com.example.social_media.dto.user.UserDto;
-import com.example.social_media.dto.user.UserProfileDto;
 
 import java.util.List;
 
 public record SearchResponseDto(
-        List<UserProfileDto> users,
+        List<UserDto> users,
         List<GroupDto> groups,
         List<PageDto> pages
 ) {}
