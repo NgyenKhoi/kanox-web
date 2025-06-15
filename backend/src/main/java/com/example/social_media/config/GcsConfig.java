f730547f690ec0331c59adb7ee7a0006599b004d
Vu Nguyen Khoi <vunguyenkhoi47@gmail.com>
2025-06-15 23:42:03 +0700

    rollback gcsconfig

--- backend/src/main/java/com/example/social_media/config/GcsConfig.java
+++ backend/src/main/java/com/example/social_media/config/GcsConfig.java
@@ -1,7 +1,5 @@
 package com.example.social_media.config;
 
-import com.google.api.client.util.Value;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.StorageOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,29 +7,12 @@ import com.google.cloud.storage.Storage;
 
 
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class GcsConfig {
 
-    @Value("${spring.cloud.gcp.credentials.location}")
-    private String credentialsLocation;
-
-    @Value("${spring.cloud.gcp.project-id}")
-    private String projectId;
-
     @Bean
-    public Storage storage() throws Exception {
-        try (InputStream credentialsStream = getClass().getResourceAsStream(credentialsLocation.replace("classpath:", "/"))) {
-            GoogleCredentials credentials = GoogleCredentials
-                    .fromStream(credentialsStream)
-                    .createScoped("https://www.googleapis.com/auth/devstorage.read_write"); // ✅ BẮT BUỘC
-
-            return StorageOptions.newBuilder()
-                    .setCredentials(credentials)
-                    .setProjectId(projectId)
-                    .build()
-                    .getService();
-        }
+    public Storage storage() throws IOException {
+        return StorageOptions.getDefaultInstance().getService();
     }
 }
