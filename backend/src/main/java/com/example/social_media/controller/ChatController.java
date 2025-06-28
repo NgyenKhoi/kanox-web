b87f9dad790266278a3ddb2c0e13eff1de4856c7
Nguyen Thao <thtn.0507@gmail.com>
2025-06-28 14:26:22 +0700

    fix

--- backend/src/main/java/com/example/social_media/controller/ChatController.java
+++ backend/src/main/java/com/example/social_media/controller/ChatController.java
@@ -216,7 +216,7 @@ public class ChatController {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         CallSession callSession = callSessionService.startCall(chatId, username);
         messagingTemplate.convertAndSend("/topic/call/" + chatId,
-                new SignalMessageDto(chatId, "start", null, null, callSession.getId()));
+                new SignalMessageDto(chatId, "start", null, null, callSession.getHost().getId()));
         return new CallSessionDto(callSession.getId(), callSession.getChat().getId(),
                 callSession.getHost().getId(), callSession.getStartTime(),
                 callSession.getEndTime());
