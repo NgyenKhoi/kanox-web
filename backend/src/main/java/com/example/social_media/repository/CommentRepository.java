301b9d3febe1d0fc9c6c1cad5e345795a99d25cc
NguyenAnn43 <anngochan2004@gmail.com>
2025-07-16 21:48:12 +0700

    add

--- backend/src/main/java/com/example/social_media/repository/CommentRepository.java
+++ backend/src/main/java/com/example/social_media/repository/CommentRepository.java
@@ -1,8 +1,14 @@
 package com.example.social_media.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.social_media.entity.Comment;
 
 import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
     List<Comment> findByPostIdAndStatusTrue(Integer postId);
+
+    int countByPostIdAndStatusTrue(Integer postId);
+
 }
