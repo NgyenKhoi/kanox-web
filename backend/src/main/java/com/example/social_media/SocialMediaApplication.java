ee98c355b4c6324f1c137d7604d244a0227f8214
Vu Nguyen Khoi <vunguyenkhoi47@gmail.com>
2025-07-22 21:59:59 +0700

    add scheduling in application

--- backend/src/main/java/com/example/social_media/SocialMediaApplication.java
+++ backend/src/main/java/com/example/social_media/SocialMediaApplication.java
@@ -3,12 +3,14 @@ package com.example.social_media;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableCaching
+@EnableScheduling
 public class SocialMediaApplication {
 
 	public static void main(String[] args) {
