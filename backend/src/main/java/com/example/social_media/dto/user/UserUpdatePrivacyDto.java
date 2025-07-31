e62d3ff7f55edc561507b4236641043eda387c50
Nguyen Thao <thtn.0507@gmail.com>
2025-07-31 10:52:36 +0700

    fix syntax

--- backend/src/main/java/com/example/social_media/dto/user/UserUpdatePrivacyDto.java
+++ backend/src/main/java/com/example/social_media/dto/user/UserUpdatePrivacyDto.java
@@ -1,11 +1,17 @@
 package com.example.social_media.dto.user;
 
 public class UserUpdatePrivacyDto {
-    private String privacySetting;
+    private String profilePrivacySetting;
     private Integer customListId;
 
-    public String getPrivacySetting() { return privacySetting; }
-    public void setPrivacySetting(String privacySetting) { this.privacySetting = privacySetting; }
+    public String getProfilePrivacySetting() {
+        return profilePrivacySetting;
+    }
+
+    public void setProfilePrivacySetting(String profilePrivacySetting) {
+        this.profilePrivacySetting = profilePrivacySetting;
+    }
+
     public Integer getCustomListId() { return customListId; }
     public void setCustomListId(Integer customListId) { this.customListId = customListId; }
 }
