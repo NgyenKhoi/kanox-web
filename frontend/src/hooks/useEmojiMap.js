2b6d3b67739c21f1f1242075381c66dba7d0a49e
Vu Huu Nguyen Khoi <162899678+NgyenKhoi@users.noreply.github.com>
2025-12-19 13:03:40 +0700

    Delete unused frontend directory

--- frontend/src/hooks/useEmojiMap.js
+++
@@ -1,6 +0,0 @@
-import { useEmojiContext } from "../context/EmojiContext";
-
-export default function useEmojiMap() {
-    const { emojiMap, loading, error } = useEmojiContext();
-    return { emojiMap, loading, error };
-}
